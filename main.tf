0d25d27dd6acad24eb78c011ebdd7f01f96af1ad
adam bekila <a.bekila18@outlook.com>
2020-09-02 19:19:23 +0000

    Feature/adding infra container

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state_bc"
+    prefix  = "terraform/state"
+  }
+}
+
+
+provider "google" {
+  version = "3.5.0"
+  project = "pacific-cab-287914"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
