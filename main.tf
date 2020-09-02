758b4783b69ccb8ec2e9ed081fc8578edfa0db52
adam bekila <a.bekila18@outlook.com>
2020-09-02 22:29:16 +0100

    cred setup

--- main.tf
+++ main.tf
@@ -20,3 +20,14 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
+
+
+resource "google_storage_bucket" "image-store" {
+  name     = "image-store-bucket"
+  location = "EU"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
