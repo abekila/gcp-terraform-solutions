19f7ac922d038f4642b66d0bb1cc145c84fbfa77
adam bekila <a.bekila18@outlook.com>
2020-09-05 16:35:21 +0000

    Feature/test

--- modules.tf
+++ modules.tf
@@ -1,22 +1,29 @@
-variable "project_id" { }
-variable "gke_cluster_name" { }
-variable "zone" { }
-variable "node_count" { }
-variable "auto_upgrade" { }
-variable "machine_type" { }
-variable "disk_size_gb" { }
+variable "project_id" {}
+variable "gke_cluster_name" {}
+variable "zone" {}
+variable "node_count" {}
+variable "auto_upgrade" {}
+variable "machine_type" {}
+variable "disk_size_gb" {}
 variable "region" {}
+variable "api_list" {}
+
+module "enable_services" {
+  source     = "./modules/enable_services"
+  project_id = var.project_id
+  api_list   = var.api_list
+}
 
 module "kubernetes_cluster" {
-  source                = "./modules/gke_cluster"
-  project_id            = var.project_id
-  gke_cluster_name      = var.gke_cluster_name
-  region                = var.region
-  zone                  = var.zone
-  node_count            = var.node_count
-  auto_upgrade          = var.auto_upgrade
-  machine_type          = var.machine_type
-  disk_size_gb          = var.disk_size_gb
+  source           = "./modules/gke_cluster"
+  project_id       = var.project_id
+  gke_cluster_name = var.gke_cluster_name
+  region           = var.region
+  zone             = var.zone
+  node_count       = var.node_count
+  auto_upgrade     = var.auto_upgrade
+  machine_type     = var.machine_type
+  disk_size_gb     = var.disk_size_gb
 }
 
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,8 +1,15 @@
-gke_cluster_name =  "bks-k8s"
+#setup 
+api_list = [
+  "cloudresourcemanager",
+  "container"
+]
+
+#gke confing val
+gke_cluster_name = "bks-k8s"
 region           = "europe-west"
-zone             =  "europe-west1-c"
-project_id       =  "bks-infra"
-node_count      =    1
-auto_upgrade     =  true
-machine_type     =  "e2-small"
-disk_size_gb     =  "40"
+zone             = "europe-west1-c"
+project_id       = "bks-infra"
+node_count       = 1
+auto_upgrade     = true
+machine_type     = "e2-small"
+disk_size_gb     = "40"
